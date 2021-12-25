3a8fdb553b836ef4065d71d16480f8e026dc497f
Github1807 <94870501+Github1807@users.noreply.github.com>
2021-12-25 21:21:51 +0530

    Dec17th 2k21, Q4

---
+++ DiamondShapeStructure.java
@@ -0,0 +1,19 @@
+package geeksterAssignments;
+
+import java.util.Scanner;
+
+public class DiamondShapeStructure {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int a=0;a<=i;a++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
