9984d8abbb7bbc9659eb6eb98876857687411e5e
Github1807 <94870501+Github1807@users.noreply.github.com>
2021-12-25 20:59:55 +0530

    Dec20th 2k21, Q1

---
+++ ArrayTakeUserInput.java
@@ -0,0 +1,14 @@
+package geeksterAssignments;
+
+import java.util.Scanner;
+
+public class ArrayTakeUserInput {
+    public static void main(String[] args) {
+        int mark[]=new int[5];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            mark[i]= sc.nextInt();
+        }
+    }
+}
+© 2021 GitHub, I
