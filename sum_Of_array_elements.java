1e333788cb5162bfa8a1e5ceb09345ab76cdae27
Github1807 <94870501+Github1807@users.noreply.github.com>
2022-01-07 10:29:49 +0530

    Jan 5th 2k22, Q1

---
+++ sum_Of_array_elements.java
@@ -0,0 +1,19 @@
+package Assignments;
+import java.util.Scanner;
+public class sum_Of_array_elements {
+    public static int arraySum(int arr[]){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int score[]=new int[10];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<10;i++){
+            score[i]= sc.nextInt();
+        }
+        System.out.println(arraySum(score));
+    }
+}
