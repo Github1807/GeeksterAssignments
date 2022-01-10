d3b5ff52c5b5b49838c9aa038b8f3ca8e6bd3b2f
Github1807 <94870501+Github1807@users.noreply.github.com>
2022-01-10 23:30:06 +0530

    Dec 13th 2k21, Q1

---
+++ last3_Numbers.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class last3_Numbers {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n= sc.nextInt();
+        int a=n%1000;
+        System.out.println(a);
+    }
+}
