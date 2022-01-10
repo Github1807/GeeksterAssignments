47299f8a0a2d7568c69daf1e0f92d9c02f8607b1
Github1807 <94870501+Github1807@users.noreply.github.com>
2022-01-10 23:33:08 +0530

    Dec 13th 2k21, Q2

---
+++ starting_endingNo.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+public class starting_endingNo {
+    public static void main(String[] args) {
+        int a= 100;
+        int first_no=0;
+        int last_no=101;
+        for(int i=1;i<51;i++){
+            first_no=first_no+1;
+            last_no=last_no-1;
+            System.out.print(first_no +","+last_no +",");
+        }
+
+
+    }
+}
